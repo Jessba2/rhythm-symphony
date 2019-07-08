4ff24fdbec1d92e387f3cae6bede85a52b602fbe
Jessica Barnett <jessba2@pdx.edu>
2019-07-07 21:05:43 -0700

    began main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//Jessica Barnett, Thomas Howell
+//Summer 2019
+//main.cpp
+//rhythm symphony
+
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+				cout << "Welcome to rhythm symphony" <<endl;
+				return 0;
+
+}
